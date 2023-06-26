a999acdc1463b18c61fdacc92007222280d13ce7
krea <krea@PC-IT-DEV>
2023-06-26 15:48:42 +0500

    Added ADUserManager and ADUser classes

---
+++ ADUserManager/ADUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADUserManager
+{
+    internal class ADUser
+    {
+        private Dictionary<string, string> _userFields;
+
+        public ADUser() 
+        {
+            _userFields = new Dictionary<string, string>();
+        }
+        public void SetField(string fieldName, string fieldValue)
+        {
+            _userFields[fieldName] = fieldValue;
+        }
+        public string GetField(string fieldName)
+        {
+            if (_userFields.ContainsKey(fieldName))
+            {
+                return _userFields[fieldName];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ADUserManager/ADUserManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADUserManager
+{
+    internal class ADUserManager
+    {
+        private List<ADUser> users;
+
+        public ADUserManager() 
+        { 
+            users = new List<ADUser>();
+        }
+        public void SearchUsers(string pattern) 
+        {
+            ADUser user = new ADUser();
+            users.Add(user);
+        }
+    }
+}
